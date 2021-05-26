751d40a6c4494771deb757f6efa375b104543b5b
edwardzhang5 <edwardzhang5@gmail.com>
2021-05-26 14:58:37 -0400

    added initial homepage

---
+++ src/components/homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class homepage extends Component {
+    render() {
+        return (
+            <div style={{ marginLeft: "80px", marginTop: "80px" } }>
+                <h1>Welcome to Edward's webpage</h1>
+                <button type="button" class="btn btn-danger">Dab</button>
+            </div>
+            
+            );
+    }
+}
+export default homepage;
