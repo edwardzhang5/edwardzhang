34baf58e4e10d7eb4c57bc05acae18f4f915e4ba
edwardzhang5 <edwardzhang5@gmail.com>
2021-05-30 18:33:58 -0400

    fixed routing issue pt2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 
