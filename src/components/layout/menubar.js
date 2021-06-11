591088d89356fb8aeec00f037fdbc84fd95c5732
edwardzhang5 <edwardzhang5@gmail.com>
2021-06-11 13:17:48 -0400

    fixed navbar

--- src/components/layout/menubar.js
+++ src/components/layout/menubar.js
@@ -1,47 +1,48 @@
 import React, { Component } from "react";
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class MenuBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" style={{ marginLeft: ".5rem" }} href="/">Edward Zhang</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2" style={{ marginLeft: "1.5rem" }}>
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item" style={{ marginLeft: "1rem" }}>
+                        <li class="nav-item ">
                             <Link to="/aboutme" class="nav-link" >About Me</Link>
                         </li>
-                        <li class="nav-item" style={{ marginLeft: "1rem" }}>
+                        <li class="nav-item " style={{ marginLeft: "1rem" }}>
                             <Link to="/project" class="nav-link">Projects</Link>
                         </li>
-
-                        <li class="nav-item" style={{ marginLeft: "1rem" }}>
-                            <a class="nav-link" href="https://drive.google.com/file/d/1OeMbBoJL4LWet48ZJ39bcWi3gdo56PtF/view?usp=sharing">Resume</a>
+                        <li class="nav-item " style={{ marginLeft: "1rem" }}>
+                            <a class="nav-link" href="https://drive.google.com/file/d/1OeMbBoJL4LWet48ZJ39bcWi3gdo56PtF/view?usp=sharing" target="_blank">Resume</a>
                         </li>
-                        
-                        <li class="nav-item dropdown" style={{ marginLeft: "1rem" }}>
+                    </ul>
+                </div>
+                <div class="mx-auto order-0">
+                    <a class="navbar-brand mx-auto" href="/">Edward Zhang</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" style={{ marginRight: "1.5rem" }}>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown" style={{ marginRight: "1rem" }} >
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Socials
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="https://www.linkedin.com/in/edwardzhang5/">LinkedIn</a>
-                                <a class="dropdown-item" href="https://github.com/edwardzhang5">GitHub</a>
-                                <a class="dropdown-item" href="https://www.facebook.com/edward.zhang.353250">Facebook</a>
-                                <a class="dropdown-item" href="https://twitter.com/edward_zhang_">Twitter</a>
+                                <a class="dropdown-item" href="https://www.linkedin.com/in/edwardzhang5/" target="_blank">LinkedIn</a>
+                                <a class="dropdown-item" href="https://github.com/edwardzhang5" target="_blank">GitHub</a>
+                                <a class="dropdown-item" href="https://www.facebook.com/edward.zhang.353250" target="_blank">Facebook</a>
+                                <a class="dropdown-item" href="https://twitter.com/edward_zhang_" target="_blank">Twitter</a>
                             </div>
                         </li>
-                        <li class="nav-item" style={{ marginLeft: "1rem" }}>
+                        <li class="nav-item">
                             <Link to="/contact" class="nav-link">Contact Me</Link>
                         </li>
-
                     </ul>
                 </div>
-               
-
             </nav>
 
         );
