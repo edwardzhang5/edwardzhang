1f2f552d0fe1449cc714488134dd8fbee9371318
edwardzhang5 <edwardzhang5@gmail.com>
2021-07-28 17:59:05 -0400

    the menu bar works now with the gg page on the router

--- src/components/assets/projects/GG.js
+++ src/components/assets/projects/GG.js
@@ -6,11 +6,12 @@ class Gatorloop extends React.Component{
     render(){
         return(
             <div>
-                <Navbar/>
+                 <Navbar/>
                 <div style={{ marginLeft: "80px", marginTop: "80px" }}>
                     <h1>Coming soon :)</h1>
                 </div>
             </div>
+
         )
     }
 }
