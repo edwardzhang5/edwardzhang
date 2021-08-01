d8a33cf6038ea3f657da89f5b3c2365e2e879152
edwardzhang5 <edwardzhang5@gmail.com>
2021-08-01 11:37:18 -0400

    added content to gatorloop ims slide

--- src/components/assets/projects/GG.js
+++ src/components/assets/projects/GG.js
@@ -5,14 +5,45 @@ import Navbar from "../Navbar";
 class Gatorloop extends React.Component{
     render(){
         return(
-            <div>
-                <div className="container">
+            <div className="container">
                     <Navbar/>
-                </div>
-                <div style={{ marginLeft: "80px", marginTop: "80px" }}>
-                    <h1>Coming soon :)</h1>
-                </div>
-            </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="mu-portfolio-area">
+                                        <div className="mu-portfolio-header">
+                                            <h2 className="mu-heading-title">Gatorloop Inventory Managment System</h2>
+                                            <h5 className="mu-heading-title">Product Owner, Full Stack Developer</h5>
+                                            <span className="mu-header-dot"></span>
+                                        </div>
+                                        <div className="mu-portfolio-content">
+                                            <div className="filtr-container">
+
+                                                <div className="col-xs-12 col-sm-12 col-md-12 filtr-item" data-category="1">
+                                                    <div className="mu-item-thumbonail">
+                                                        <img className="img-responsive" src="assets/images/portfolio/GG1.gif" alt="image" />
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div className="col-xs-12 col-sm-12 col-md-12 filtr-item" data-category="1">
+                                                    <div className="mu-item-thumbonail">
+                                                        <img className="img-responsive" src="assets/images/portfolio/GG2.gif" alt="image" />
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div className="col-xs-12 col-sm-12 col-md-12 filtr-item" data-category="1">
+                                                    <div className="mu-item-thumbonail">
+                                                        <img className="img-responsive" src="assets/images/portfolio/GG3.gif" alt="image" />
+                                                    </div>
+                                                    
+                                                </div>                                               
+
+                                            </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
         )
     }
