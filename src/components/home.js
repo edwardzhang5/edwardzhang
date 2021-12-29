31c8fb59135f62ae68b44e35c1e27434e746906e
edwardzhang5 <edwardzhang5@gmail.com>
2021-12-29 10:09:48 -0500

    update style

--- src/components/home.js
+++ src/components/home.js
@@ -1,61 +1,54 @@
-import React from 'react';
-import FeaturedSlider from './FeaturedSlider';
-import About from './About';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Navbar from './assets/Navbar';
-
+import React from 'react'
+import FeaturedSlider from './FeaturedSlider'
+import About from './About'
+import Portfolio from './Portfolio'
+import Contact from './Contact'
+import Navbar from './assets/Navbar'
 
 class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Start Header */}
+        <header id='mu-header' className='' role='banner'>
+          <div className='container'>
+            <Navbar />
+          </div>
+        </header>
+        {/* End Header */}
 
-    render() {
-        return (
-            <div>
-                {/* Start Header */}
-                <header id="mu-header" className="" role="banner">
-                    <div className="container">
-                        <Navbar/>
-                    </div>
-                </header>
-                {/* End Header */}
+        {/* Start Featured Slider */}
 
-                {/* Start Featured Slider */}
+        <section id='mu-featured-slider'>
+          <FeaturedSlider />
+        </section>
 
-                <section id="mu-featured-slider">
-                    <FeaturedSlider/>
-                </section>
+        {/* Start main content */}
 
-                
-                {/* Start main content */}
+        <main role='main'>
+          {/*  Start About */}
+          <section id='mu-about'>
+            <About />
+          </section>
+          {/* End About */}
 
-                <main role="main">
+          {/* Start Portfolio */}
+          <section id='mu-portfolio'>
+            <Portfolio />
+          </section>
+          {/* End Portfolio */}
 
-                    {/*  Start About */}
-                    <section id="mu-about">
-                        <About/>
-                    </section>
-                    {/* End About */}
+          {/* Start Contact */}
+          <section id='mu-contact'>
+            <Contact />
+          </section>
+          {/* End Contact */}
+        </main>
 
-                    {/* Start Portfolio */}
-                    <section id="mu-portfolio">
-                        <Portfolio/>
-                    </section>
-                    {/* End Portfolio */}
-
-                    {/* Start Contact */}
-                    <section id="mu-contact">
-                        <Contact/>
-                    </section>
-                    {/* End Contact */}
-
-                </main>
-
-                {/* End main content */}
-
-
-            </div>
-        )
-    }
+        {/* End main content */}
+      </div>
+    )
+  }
 }
 
-export default Home;
+export default Home
