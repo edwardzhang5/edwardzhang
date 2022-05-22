06137ce8e1d1043633170dd67e940db33da5d5e0
edwardzhang5 <edwardzhang5@gmail.com>
2022-05-21 23:59:07 -0400

    backend test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Gatorloop from './components/assets/projects/GG';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
     const [data, setData] = React.useState(null);
